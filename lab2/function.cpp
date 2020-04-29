c8d7f676a708701fba87712e25486e82626f649e
Nikita Bubriak <31805409+nikitabubriak@users.noreply.github.com>
2020-04-29 13:40:24 +0300

    Added input_data function
    This function looks for .csv files in selected directory and inputs their data to country and votes arrays.

--- lab2/function.cpp
+++ lab2/function.cpp
@@ -1,7 +1,73 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-#include <filesystem>
+#include <experimental/filesystem>
 
 using namespace std;
-namespace fs = std::filesystem;
+namespace fs = experimental::filesystem;
+
+void input_data(string country[n], int votes[n][n])
+{
+	string	directory, 
+			extension = ".csv", 
+			c_element, 
+			v_element;
+
+	int		c_current, 
+			c_processed = 0;
+
+	cout << "Enter " << extension << " files directory:" << endl;
+	cin >> directory;
+
+	for (auto &f : fs::directory_iterator(directory))
+	{
+		if (f.path().extension().string() == extension)
+		{
+			ifstream inFile(f.path());
+			if (!inFile.is_open()) { cout << "Error. Can not open file\n"; return; }
+			
+			inFile >> c_current;
+			for (int i = 0 + c_processed; i < c_current + c_processed; i++)
+			{
+				getline(inFile, c_element, ',');
+				country[i] = c_element;
+
+				for (int j = 0; j < n - 1; j++)
+				{
+					getline(inFile, v_element, ',');
+					votes[i][j] = stoi(v_element);
+				}
+
+				getline(inFile, v_element, '\n');
+				votes[i][n - 1] = stoi(v_element);
+			}
+
+			c_processed += c_current;
+			inFile.close();
+		}
+		else continue;
+	}
+}
+
+	//===========================test
+	//#include <iomanip>
+	
+
+	/*for (int j = 0; j < n; j++)
+	{
+		cout << setw(10) << country[j];
+		cout << "\n" << endl;
+	}
+	
+
+
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			cout << setw(10) << votes[i][j];
+			
+		}
+		cout << "\n" << endl;
+	}
+}*/
--- lab2/function.h
+++ lab2/function.h
@@ -5,4 +5,18 @@
 #include <string>
 #include <filesystem>
 
+//#include <iomanip>
+
 using namespace std;
+
+const int n = 20; //number of countries
+string country[n];
+int votes[n][n];
+int score[n] = { 0 };
+
+void input_data(string[n], int[n][n]);
+void count_score(string[n], int[n][n], int[n]);
+void enum_top10();
+void output_top10();
+
+#include "function.cpp"
